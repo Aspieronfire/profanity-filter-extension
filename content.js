fa747a3009e6f05c9a0ef821ce6495ed5d57b8af
Jacob Tracy <jacob.tracy1988@gmail.com>
2023-03-13 23:14:44 -0600

    fixed issues

--- content.js
+++ content.js
@@ -1,5 +1,23 @@
-var profanityList = ["profanity1", "profanity2", "profanity3"]; // You can replace this with your own list of profanity
-var regex = new RegExp("\\b(" + profanityList.join("|") + ")\\b", "gi");
+var regex = new RegExp(
+  '\\b(' + [
+    'a*s{1,2}(?!umption)\\b', // matches 'ass', 'asses', but not 'assumption'
+    'b[i!1]tch\\b', // matches 'bitch' or 'bit*h'
+    'd[a@]mn\\b', // matches 'damn' or 'dam'
+    'd[i!1]c?k\\b', // matches 'dick' or 'dic'
+    'f[u*]ck\\b', // matches 'fuck' or 'f*ck'
+    's[h*][i!1][t+e*]\\b', // matches 'shit', 'shite', 'shitty', 'shitey', etc.
+    'p[i!1]ss\\b', // matches 'piss' or 'pis'
+    'c[u*]nt\\b', // matches 'cunt' or 'c*nt'
+    'w[h*][o0]re\\b', // matches 'whore', 'w0re', 'wh0re', etc.
+    'b[a@4][s$5]t[a@4]rd\\b', // matches 'bastard', 'ba5tard', 'b@stard', etc.
+    'd[o0]uche[b@]ag\\b', // matches 'douchebag', 'd0uchebag', 'doucheb@g', etc.
+    'a[s$5][s$5]h[o0]le\\b', // matches 'asshole', 'a$$hole', 'ash0le', etc.
+    'm[o0]th[e*]rf[u*]ck[e*]r\\b', // matches 'motherfucker', 'm0therfucker', etc.
+    't[wu]at\\b', // matches 'twat' or 'twut'
+    't[i!1]ts?\\b' // matches 'tits' or 'tit'
+  ].join('|') + ')',
+  'gi'
+);
 
 function filterProfanity() {
   var elements = document.getElementsByTagName("*");
@@ -9,7 +27,7 @@ function filterProfanity() {
 
     if (element.nodeName === "INPUT" || element.nodeName === "TEXTAREA") {
       var oldValue = element.value;
-      var newValue = oldValue.replace(regex, "***");
+      var newValue = oldValue.replace(regex, "profanity has been replaced");
 
       if (newValue !== oldValue) {
         element.value = newValue;
